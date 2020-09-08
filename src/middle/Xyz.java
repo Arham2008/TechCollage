d8091890dee830a1affdb8f4e2c69c59901cf5eb
arham2008 <akbarmunner@gmail.com>
2020-09-07 21:24:13 -0400

    i update code

--- src/middle/Xyz.java
+++ src/middle/Xyz.java
@@ -5,7 +5,7 @@ public class Xyz {
 	//static variable
 	
 	public static int speed=345;
-	
+   public static char mysize='L';
 	
 	//non static variable
 	
@@ -14,7 +14,8 @@ public class Xyz {
 	
 	public static void main(String[] args) {
 		System.out.println(Xyz.speed);
-
+        System.out.println(Xyz.mysize);
+		
 		
 		Xyz truck=new Xyz();
 		System.out.println(truck.myMessage);
